9d70d8d706631f81ea1ffff6b35c414241d3af2b
Plech0 <filip321997@gmail.com>
2018-11-25 15:23:00 +0100

    db fill

---
+++ fill_DB.sql
@@ -0,0 +1,75 @@
+INSERT INTO user VALUES (0, 'Tomas', 'Lapsansky', 'tomas@gmail.com', '12345', 'admin', 'Presov', 'presovska 22', 00000, 'slovakia', 0 );
+INSERT INTO user VALUES (1, 'Filip', 'Plesko', 'filip@gmail.com', '12345', 'admin', 'Martin', 'tulska 9', 03601, 'slovakia', 0 );
+INSERT INTO user VALUES (2, 'Juro', 'Mrkvicka', 'juro@gmail.com', '12345', 'user', 'Zilina', 'zilinska 22', 00001, 'slovakia', 1 );
+INSERT INTO user VALUES (3, 'Jana', 'Dolna', 'jana@gmail.com', '12345', 'user', 'Bratislava', 'priehradska 2', 00002, 'slovakia', 0 );
+INSERT INTO user VALUES (4, 'Patricia', 'Hranata', 'pata@gmail.com', '12345', 'user', 'Brno', 'kolejni 2', 11100, 'czech republic', 0 );
+INSERT INTO user VALUES (5, 'Jozef', 'jozo@gmail.com', '12345', 'user', 'Prievidza', 'pod grunom 5', 53802, 'slovakia', 1 );
+
+
+INSERT INTO drug VALUES (0, 'Paralen 500', 36, 0, 10, 'Paracetamol, léčivá látka přípravku Paralen 500 tablety, působí proti bolesti a snižuje zvýšenou tělesnou teplotu.');
+INSERT INTO drug VALUES (1, 'Ibalgin 400', 59, 1, 13, 'Ibalgin 400 zmírňuje bolest a zánět různého původu a zároveň tlumí horečku provázející nemoci z nachlazení u dospělých a dospívajících od 12 let.');
+INSERT INTO drug VALUES (2, 'Endiaron', 20, 0, 5, 'Endiaron tablety jsou střevní dezinficiens používané k léčbě infekčních průjmů u dospělých, dospívajících a dětí o tělesné hmotnosti vyšší než 40 kg.');
+INSERT INTO drug VALUES (3, 'Laktobacily 6', 15, 1, 12, 'Komplex laktobacilů a bifidobakterií: 6 kmenů, 5 miliard mikroorganismů.');
+INSERT INTO drug VALUES (4, 'Stoptussin sirup 180 ml', 78, 1, 5, 'Přípravek tlumí dráždění ke kašli a mění skladbu průduškového hlenu a usnadňuje tak jeho vykašlávání. Sirup k léčbě suchého, dráždivého a obtížně ztišitelného kašle.');
+INSERT INTO drug VALUES (5, 'Olfen gel 100 g', 35, 2, 8, 'K léčbě bolestí spojených s akutním natažením nebo pohmožděním.');
+INSERT INTO drug VALUES (6, 'Orlistat Sandoz 60 mg', 24, 2, 16, 'Přípravek se používá ke snížení tělesné hmotnosti u dospělých od 18 let s nadváhou, kteří mají index BMI rovný nebo vyšší než 28. Přípravek Orlistat Sandoz 60 mg se má užívat společně s dietou se sníženým obsahem kalorií a nízkým obsahem tuků.');
+INSERT INTO drug VALUES (7, 'Boiron Oscillococcinum', 43, 1, 20, 'Homeopatický léčivý přípravek.');
+INSERT INTO drug VALUES (8, 'Glyvenol 400', 43, 2, 15, 'Glyvenol 400 se používá k léčbě onemocnění žilního krevního oběhu a u hemoroidů u dospělých a dospívajících od 12 let.');
+INSERT INTO drug VALUES (9, 'Stoptussin sirup 180 ml', 54, 1, 5, 'Přípravek tlumí dráždění ke kašli a mění skladbu průduškového hlenu a usnadňuje tak jeho vykašlávání. Sirup k léčbě suchého, dráždivého a obtížně ztišitelného kašle.');
+INSERT INTO drug VALUES (10, 'ACC LONG 600 mg 20', 10, 1, 7, 'ACC LONG usnadňuje vykašlávání. Užívá se k léčbě vlhkého kašle. Přípravek mohou užívat dospělí a děti starší než 14 let.');
+INSERT INTO drug VALUES (11, 'Visine Classic oční kapky 15 ml', 12, 2, 4, 'Spolehlivě odstraní zarudnutí očí. Kapky jsou vhodné také k léčbě alergických obtíží.');
+INSERT INTO drug VALUES (12, 'Vidisic Oční gel 3x10 g', 15, 1, 8, 'Přípravek se používá jako náhrada slzní tekutiny při nedostatečné produkci slz, při léčbě příznaků suchého oka (suchý zánět spojivek) a jako ochrana proti dalšímu podráždění.');
+INSERT INTO drug VALUES (13, 'Ophthalmo-Septonex oční mast 5 g', 3, 1, 3, 'Ophthalmo-Septonex oční mast se používá při léčbě nehnisavých zánětů spojivek a očních víček, při poškození rohovky cizím tělískem u dospělých i dětí.');
+INSERT INTO drug VALUES (14, 'Prostamol uno 320 mg', 6, 2, 22, 'Prostamol Uno obsahuje skupinu látek, které působí různými mechanismy na zbytnělou prostatu a zlepšují močové obtíže. Přípravky s extraktem z plodů Serenoa repens nenarušují sexuální funkce.');
+INSERT INTO drug VALUES (15, 'Prostakan Forte', 68, 2, 21, 'Prostakan forte se používá při benigní hyperplazii (nezhoubném zbytnění) prostaty, především její počáteční stadia (stadium I - II). Jsou nutné pravidelné kontroly lékařem - urologem.');
+INSERT INTO drug VALUES (16, 'Fenistil kapky 20 ml', 54, 2, 5, 'Fenistil kapky se používají k léčbě příznaků alergie (kožní vyrážky, svědění, senná rýma, jiné druhy alergické rýmy) u dospělých, dospívajících a dětí od 1 roku.');
+INSERT INTO drug VALUES (18, 'Analergin 10 mg', 54, 1, 7, 'Analergin se používá k zmírnění nosních a očních příznaků sezónní a celoroční alergické rýmy a ke zmírnění příznaků chronické kopřivky nejasného původu.');
+INSERT INTO drug VALUES (19, 'Allergodil nosní sprej 10 ml', 75, 1, 9, 'Allergodil nosní sprej se používá k léčbě nosních příznaků sezónní alergické rýmy (senné rýmy) a celoroční alergické rýmy.');
+INSERT INTO drug VALUES (20, 'Hylak forte roztok 100 ml', 47, 2, 8, 'Hylak Forte se užívá při nadýmání, průjmu nebo zácpě, poruchách trávení, při trávicích obtížích u dospělých i dětí včetně kojenců.');
+INSERT INTO drug VALUES (21, 'Pangrol 20000', 25, 1, 9, 'Pangrol 20 000 se používá u chorob slinivky břišní, provázených poruchou její zevní sekrece (např. při zánětech slinivky břišní, mukoviscidóze), po operacích žaludku a tenkého střeva (resekcích) a při poruchách vylučování žlučí.');
+INSERT INTO drug VALUES (22, 'Rennie 96 žvýkacích tablet', 1, 2, 7, 'Rychlá a bezpečná* úleva od pálení žáhy. Při pálení žáhy, při překyselení žaludku a pocitu plnosti.');
+INSERT INTO drug VALUES (23, 'Apo-Ome 14 tablet', 15, 1, 6, 'APO-OME 20 se užívá na úlevu od příznaků pálení žáhy a regurgitace (návrat kyselého žaludečního obsahu zpět do jícnu).');
+INSERT INTO drug VALUES (24, 'Famosan 10 mg', 78, 2, 2, 'Famosan 10 mg tablety se užívají ke krátkodobé léčbě žaludečních obtíží (příznaky žaludečního překyselení, pálení žáhy a nevolnosti) u dospělých.');
+INSERT INTO drug VALUES (25, 'Persen 40 obalených table', 15, 1, 7, 'Léčivý přípravek s tradiční kombinací bylin se zklidňujícím účinkem (kozlík, meduňka, máta).');
+
+
+INSERT INTO insurer VALUES (0, 'Generali Poisťovňa, a. s.');
+INSERT INTO insurer VALUES (1, 'UNION poisťovňa, a.s.');
+
+
+INSERT INTO producer VALUES (0, 'Phoenix Zdravotnícke zásobovanie, a.s.');
+INSERT INTO producer VALUES (1, 'IBSA Slovakia, s.r.o.');
+
+
+INSERT INTO order VALUES (0, 'waiting', 'Zilina', '00001', 'zilinska 22');
+INSERT INTO order VALUES (1, 'sended', 'Bratislava', '00002', 'priehradska 2');
+INSERT INTO order VALUES (2, 'sended', 'Brno', '11100', 'kolejni 2');
+INSERT INTO order VALUES (3, 'sended', 'Prievidza', '53802', 'pod grunom 5');
+INSERT INTO order VALUES (5, 'waiting', 'Zilina', '00001', 'zilinska 22');
+INSERT INTO order VALUES (6, 'sended', 'Zilina', '00001', 'zilinska 22');
+INSERT INTO order VALUES (7, 'waiting', 'Prievidza', '53802', 'pod grunom 5');
+INSERT INTO order VALUES (8, 'waiting', 'Zilina', '00001', 'zilinska 22');
+INSERT INTO order VALUES (9, 'sended', 'Prievidza', '53802', 'pod grunom 5');
+INSERT INTO order VALUES (10, 'waiting', 'Zilina', '00001', 'zilinska 22');
+INSERT INTO order VALUES (11, 'sended', 'Zilina', '00001', 'zilinska 22');
+
+
+
+INSERT INTO drug_insurer VALUES (0, 5, 1, 0);
+INSERT INTO drug_insurer VALUES (1, 12, 3, 0);
+INSERT INTO drug_insurer VALUES (2, 5, 2, 1);
+INSERT INTO drug_insurer VALUES (3, 5, 4, 1);
+
+
+INSERT INTO order_drug VALUES (0, 5, 3, 0);
+INSERT INTO order_drug VALUES (1, 5, 0, 1);
+INSERT INTO order_drug VALUES (2, 3, 1, 2);
+INSERT INTO order_drug VALUES (3, 4, 2, 3);
+INSERT INTO order_drug VALUES (4, 4, 13, 4);
+INSERT INTO order_drug VALUES (5, 3, 15, 5);
+INSERT INTO order_drug VALUES (6, 2, 12, 6);
+INSERT INTO order_drug VALUES (7, 7, 5, 7);
+INSERT INTO order_drug VALUES (8, 5, 25, 8);
+INSERT INTO order_drug VALUES (9, 8, 20, 9);
+INSERT INTO order_drug VALUES (10, 11, 18, 10);
+INSERT INTO order_drug VALUES (11, 17, 17, 11);
