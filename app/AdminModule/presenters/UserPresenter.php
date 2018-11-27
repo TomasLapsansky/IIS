1023bc5585101bf84b866426407e1be4057ac3d0
tomas.lapsansky.97@gmail.com <x3vNLiGd>
2018-11-27 20:25:26 +0100

    user add + edit

--- app/AdminModule/presenters/UserPresenter.php
+++ app/AdminModule/presenters/UserPresenter.php
@@ -4,12 +4,9 @@ namespace App\AdminModule\Presenters;
 
 
 use App\Presenters\BasePresenter;
-use Models\User;
+use Nette\Application\UI;
 
 final class UserPresenter extends BasePresenter {
-
-    /** @var User @inject */
-    public $userService;
 
     public function actionDefault() {
         if ($this->userService->count() != 0) {
@@ -27,8 +24,126 @@ final class UserPresenter extends BasePresenter {
     }
 
     public function actionEdit($id) {
-        $this->template->id = $id;
-        $this->template->sys_user = $this->userService->getByID($id);
+        $sys_user = $this->userService->getByID($id);
+        $this->template->sys_user = $sys_user;
+
+        $this['editForm']->setDefaults([
+            'name' => $sys_user->name,
+            'surname' => $sys_user->surname,
+            'email' => $sys_user->email,
+            //'role' => $sys_user->role,
+            'city' => $sys_user->city,
+            'address' => $sys_user->address,
+            'zip' => $sys_user->zip,
+            'country' => $sys_user->country,
+            'insurer' => $sys_user->insurer_id,
+            'id' => $sys_user->id
+        ]);
+
+        if($sys_user->role == 'user') {
+            $this['editForm']->setDefaults(['role' => 0]);
+        } elseif($sys_user->role == 'admin') {
+            $this['editForm']->setDefaults(['role' => 1]);
+        }
+    }
+
+    protected function createComponentAddForm()
+    {
+        $insurers = $this->insurerService->getAll();
+
+        $form = new UI\Form();
+        $form->addText('name', 'Meno:')->setRequired();
+        $form->addText('surname', 'Priezvisko:')->setRequired();
+        $form->addEmail('email', 'Email:')->setRequired();
+        $form->addPassword('password', 'Heslo:')->setRequired();
+        $form->addSelect('role', 'Rola:', [
+            'user',
+            'admin'
+        ])->setRequired();
+        $form->addText('city', 'Mesto:')->setRequired();
+        $form->addText('address', 'Adresa:')->setRequired();
+        $form->addText('zip', 'ZIP:')->setRequired();
+        $form->addText('country', 'Stat:')->setRequired();
+        $form->addSelect('insurer', 'Poistovna:', $insurers->fetchPairs('id', 'name'))->setRequired();
+        $form->addSubmit('add', 'Add');
+        $form->onSuccess[] = [$this, 'addFormSucceeded'];
+        return $form;
+    }
+
+    public function addFormSucceeded(UI\Form $form, $values)
+    {
+        if($values->role == 'user') {
+            $role = 'user';
+        } elseif($values->role == 'admin') {
+            $role = 'admin';
+        }
+
+        $this->userService->insert([
+            'name' => $values->name,
+            'surname' => $values->surname,
+            'email' => $values->email,
+            'password' => $values->password,
+            'role' => $role,
+            'city' => $values->city,
+            'address' => $values->address,
+            'zip' => $values->zip,
+            'country' => $values->country,
+            'insurer_id' => $values->insurer
+        ]);
+
+        $this->redirect('User:');
+    }
+
+    protected function createComponentEditForm()
+    {
+        $insurers = $this->insurerService->getAll();
+
+        $form = new UI\Form();
+        $form->addText('name', 'Meno:')->setRequired();
+        $form->addText('surname', 'Priezvisko:')->setRequired();
+        $form->addEmail('email', 'Email:')->setRequired();
+        $form->addPassword('password', 'Heslo:');
+        $form->addSelect('role', 'Role:', [
+            'user',
+            'admin'
+        ])->setRequired();
+        $form->addText('city', 'Mesto:')->setRequired();
+        $form->addText('address', 'Adresa:')->setRequired();
+        $form->addText('zip', 'ZIP:')->setRequired();
+        $form->addText('country', 'Stat:')->setRequired();
+        $form->addSelect('insurer', 'Poistovna:', $insurers->fetchPairs('id', 'name'))->setRequired();
+        $form->addSubmit('edit', 'Edit');
+        $form->addHidden('id');
+        $form->onSuccess[] = [$this, 'editFormSucceeded'];
+        return $form;
+    }
+
+    public function editFormSucceeded(UI\Form $form, $values)
+    {
+        $sys_user = $this->userService->getByID($values->id);
+
+        $sys_user->update([
+            'name' => $values->name,
+            'surname' => $values->surname,
+            'email' => $values->email,
+            'city' => $values->city,
+            'address' => $values->address,
+            'zip' => $values->zip,
+            'country' => $values->country,
+            'insurer_id' => $values->insurer
+        ]);
+
+        if($values->role == 'user') {
+            $sys_user->update(['role' => 'user']);
+        } elseif($values->role == 'admin') {
+            $sys_user->update(['role' => 'admin']);
+        }
+
+        if($values->password) {
+            $sys_user->update(['password' => $values->password]);
+        }
+
+        $this->redirect('User:');
     }
 
 }
