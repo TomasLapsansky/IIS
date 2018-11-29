299480316acf85c3b7233170e0d59a43eae9935f
Plech0 <filip321997@gmail.com>
2018-11-29 18:09:53 +0100

    user-settings/product-detail/user-orders

--- app/presenters/ProductPresenter.php
+++ app/presenters/ProductPresenter.php
@@ -6,7 +6,11 @@ namespace App\Presenters;
 final class ProductPresenter extends BasePresenter {
 
     public function actionDefault() {
-
+        $this->template->products = $this->productService->getAll();
     }
 
+    public function actionDetail($id) {
+        $this->template->id = $id;
+        $this->template->product = $this->productService->getByID($id);
+    }
 }
