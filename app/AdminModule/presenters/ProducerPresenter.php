cdf1c666faba1c78ee9749d959db3175bbeab4c2
Plech0 <filip321997@gmail.com>
2018-11-30 11:42:00 +0100

    add drug to insurer/producer, create producer, show product in order

--- app/AdminModule/presenters/ProducerPresenter.php
+++ app/AdminModule/presenters/ProducerPresenter.php
@@ -2,8 +2,25 @@
 
 namespace App\AdminModule\Presenters;
 
+use App\Presenters\BasePresenter;
+use Models\Producer;
+use Nette;
 
 final class ProducerPresenter extends AdminBasePresenter {
+    
+    /** @var Producer @inject */
+    public $producerService;
+
+    public function actionDefault() {
+        if ($this->producerService->count() != 0) {
+            $this->template->producers = $this->producerService->getAll();
+        }
+    }
+
+    public function actionDetail($id) {
+        $this->template->id = $id;
+        $this->template->insurer = $this->insurerService->getByID($id);
+    }
 
 
 
